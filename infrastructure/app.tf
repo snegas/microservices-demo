50591ff31fdcd1f19382c3caabe1f8d2cd26b840
Alexander Snegovoy <9533852+snegas@users.noreply.github.com>
2024-02-22 05:53:42 +0000

    fixes

--- infrastructure/app.tf
+++ infrastructure/app.tf
@@ -19,24 +19,31 @@ locals {
   current_redis_settings = local.all_redis_settings[terraform.workspace]
 }
 
-resource "digitalocean_database_cluster" "redis" {
-  count      = local.is_app
-  name       = "${local.prefix}-redis"
-  engine     = "redis"
-  version    = "7"
-  size       = local.current_redis_settings["size"]
-  region     = "nyc1"
-  node_count = local.current_redis_settings["node_count"]
+data "digitalocean_vpc" "shared" {
+  count = local.is_app
+  name  = "${local.prefix}-vpc"
 }
 
-data "digitalocean_project" "environment" {
+
+resource "digitalocean_database_cluster" "redis" {
+  count                = local.is_app
+  name                 = "${local.prefix}-redis"
+  engine               = "redis"
+  version              = "7"
+  size                 = local.current_redis_settings["size"]
+  region               = "nyc1"
+  node_count           = local.current_redis_settings["node_count"]
+  private_network_uuid = data.digitalocean_vpc.shared[0].id
+}
+
+data "digitalocean_project" "shared" {
   count = local.is_app
   name  = terraform.workspace
 }
 
 resource "digitalocean_project_resources" "app" {
   count   = local.is_app
-  project = data.digitalocean_project.environment[0].id
+  project = data.digitalocean_project.shared[0].id
   resources = [
     digitalocean_database_cluster.redis[0].urn
   ]
