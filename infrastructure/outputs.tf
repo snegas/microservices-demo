022fbc14e6f28610397cb45b675742e31910ebfa
Alexander Snegovoy <9533852+snegas@users.noreply.github.com>
2024-02-22 03:16:18 +0000

    fixes

--- infrastructure/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "account_status" {
-    value = data.digitalocean_account.current.status
-    description = "Status of the current account"
-}
--- infrastructure/project.tf
+++ infrastructure/project.tf
@@ -1,14 +1,16 @@
 locals {
   environment_base = {
-    dev = "Development"
+    dev   = "Development"
     stage = "Staging"
-    prod = "Production"
+    prod  = "Production"
   }
 
   is_shared = var.type == "shared" ? 1 : 0
-  env = local.environment_base[terraform.workspace]
+  env       = local.environment_base[terraform.workspace]
 
   prefix = "${terraform.workspace}-${var.type}-${var.prefix}"
+
+  base_domain = terraform.workspace == "prod" ? var.base_domain : "${terraform.workspace}.${var.base_domain}"
 }
 
 resource "digitalocean_project" "environment" {
@@ -17,4 +19,23 @@ resource "digitalocean_project" "environment" {
   description = "A project to represent ${local.env} resources."
   purpose     = "Web Application"
   environment = local.env
+}
+
+resource "digitalocean_vpc" "default" {
+  count  = local.is_shared
+  name   = "${local.prefix}-vpc"
+  region = "nyc3"
+}
+
+resource "digitalocean_domain" "default" {
+  count = var.type == "shared" && var.base_domain != 0 > 0 ? 1 : 0
+  name  = local.base_domain
+}
+
+resource "digitalocean_project_resources" "environment" {
+  project = digitalocean_project.environment.id
+  resources = [
+    digitalocean_vpc.default.urn,
+    digitalocean_domain.default.urn
+  ]
 }
